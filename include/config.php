f701e07ef460b8005f4fb159e1172c8b5ef6327d
Caleb Lawson <caleb@lawson.rocks>
2015-01-05 16:35:11 -0600

    Initial commit.

---
+++ include/config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'test');
+define('DB_PASSWORD', 'test');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'SeniorProject');
+ 
+define('USER_CREATED_SUCCESSFULLY', 0);
+define('USER_CREATE_FAILED', 1);
+define('USER_ALREADY_EXISTED', 2);
+?>
